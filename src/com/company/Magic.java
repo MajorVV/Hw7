fb8ce3388456ab0a78b1e966fa9b782317998f98
MajorVV <stabvtc@gmail.com>
2022-04-17 23:35:36 +0600

    Initial commit

---
+++ src/com/company/Magic.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Magic extends Hero {
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        setDamage(25);
+        setHealth(300);
+        setSuperAbilityType(" Magic применил свою суперспособность!");
+        System.out.println("HP: " + getHealth() + "| DAMAGE:" + getDamage() + "|" + getSuperAbilityType());
+
+    }
+}
